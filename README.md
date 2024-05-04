41824fe504138885e08443fc5b3fa86113b6d88f
Devisson Vasquez <devissonvasquez99@gmail.com>
2024-05-03 19:34:10 -0500

    firts commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+## Dev
+1. Clonar el repositorio
+2. Instalar dependencias `npm install`
+3. Crear un archivo `.env` basado en el `env.template`
+4. Ejecutar migracion de prisma `npx prisma migrate dev`
+5. Ejecutar contenedor para correr la BD `docker compose up -d`
+6. Ejecutar `npm run start:dev`
---
+++ src/orders/dto/create-order.dto.ts
@@ -0,0 +1,31 @@
+import { OrderStatus } from "@prisma/client";
+import { 
+    IsEnum, 
+    IsNumber, 
+    IsPositive, 
+    IsOptional, 
+    IsBoolean
+} from 'class-validator';
+import { OrderStatusList } from "../enum/order.enum";
+
+export class CreateOrderDto {
+    
+    @IsNumber()
+    @IsPositive()
+    totalAmount: number;
+
+    @IsNumber()
+    @IsPositive()
+    totalItems: number;
+
+    @IsEnum(OrderStatusList, {
+        message: `Possible status values are ${ OrderStatusList }`
+    })
+    @IsOptional()
+    status: OrderStatus = OrderStatus.PENDING
+
+    @IsBoolean()
+    @IsOptional()
+    paid: boolean;
+
+}
